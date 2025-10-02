96c49937a6e4225dfb8cc8d3c26da3e71e124337
fabrisdev <ezepuntocom@hotmail.com>
2025-10-02 13:28:23 -0300

    More testing

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -6,8 +6,8 @@ var distance_x := [0]
 func _process(delta: float) -> void:
 	$Camera3D.look_at($MakotosBigWackyHead.global_position)
 	ImGui.Begin("Debug window")
-	if ImGui.SliderInt("FOV", fov, 0, 180):
+	if ImGui.SliderFloat("FOV", fov, 0, 180):
 		$Camera3D.fov = fov[0]
-	if ImGui.SliderFloat("Distance", distance_x, -10, 10):
+	if ImGui.SliderFloat("Distance", distance_x, -1, 6):
 		$Camera3D.position.x = distance_x[0]
 	ImGui.End()
