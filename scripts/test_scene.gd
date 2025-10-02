b0146061fe08d27b54e6c6f30473c5c59790749b
fabrisdev <ezepuntocom@hotmail.com>
2025-10-02 13:17:58 -0300

    Added ImGui to do testing easier

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
+var fov := [70]
+var distance_x := [0]
+
 func _process(delta: float) -> void:
 	$Camera3D.look_at($MakotosBigWackyHead.global_position)
-
-func _ready() -> void:
-	$AnimationPlayer.play_backwards("new_animation")
-	
-
-func close():
-	get_tree().quit()
+	ImGui.Begin("Debug window")
+	if ImGui.SliderInt("FOV", fov, 0, 180):
+		$Camera3D.fov = fov[0]
+	if ImGui.SliderFloat("Distance", distance_x, -10, 10):
+		$Camera3D.position.x = distance_x[0]
+	ImGui.End()
