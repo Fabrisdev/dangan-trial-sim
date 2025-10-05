91655e9b462f2bd666332411428fbbb70e0ba9df
fabrisdev <ezepuntocom@hotmail.com>
2025-10-05 19:04:30 -0300

    nose

--- script-generator/my_script.ts
+++ script-generator/my_script.ts
@@ -4,9 +4,9 @@ import { bold, narrate, think } from "dangan-trial-maker/bb";
 const hina = actor("hina", 0);
 hina.say(
 	narrate(
-		"You'll have to rely on your own ",
+		"You'll have to rely on your own",
 		bold("logic"),
-		" to determine which weak spots are actually ",
+		"to determine which weak spots are actually",
 		bold("lies or contradictions"),
 		".",
 	),
