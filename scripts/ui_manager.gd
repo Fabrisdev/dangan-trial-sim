8d21e9d0a97f91ba43869defa6148c53647c6b9c
fabrisdev <ezepuntocom@hotmail.com>
2025-10-09 22:59:54 -0300

    nose

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -6,7 +6,8 @@ var regex := RegEx.new()
 const actors_to_names := {
 	"hina": "Aoi Asahina",
 	"hifumi": "Hifumi Yamada",
-	"makoto": "Makoto Naegi"
+	"makoto": "Makoto Naegi",
+	"narrator": "Narrator"
 }
 
 
@@ -26,16 +27,30 @@ func show_text(actor: String, text: String) -> void:
 			text_to_show += "\n"
 		else:
 			text_to_show += " "
+		##aqui pasteo su codigo
+		var style_check:bool #para saber en que momento es que hay un estilo
+		$Text.text = word
+		var style_todo:String #Tomará el string cuando style_check esté activo
 		for letter in word:
-			text_to_show += letter
+			if letter == '[':
+				style_check=true
 			$Text.text = text_to_show.strip_edges()
-			await get_tree().create_timer(text_speed).timeout
+			if style_check: # llenando el formato de estilos
+				style_todo += letter
+			else: 
+				text_to_show += letter
+				await get_tree().create_timer(text_speed).timeout
+			if letter == ']': #si se detecta el inicio de corchetes con los estilos activos
+				text_to_show += style_todo
+				style_check=false
+				style_todo = '' #cuando ponga el estilo,borrará formato
 	get_parent().set_can_skip(true)
 	$MouseButton.visible = true
 	
 func show_face(actor: String) -> void:
 	var actor_to_face_id := {
 		"makoto": 0,
+		"narrator": 0,
 		"hifumi": 5,
 		"hina": 9
 	}
