18e3e4f77c43b20a0c4b21c638400f44073dd423
fabrisdev <ezepuntocom@hotmail.com>
2025-10-21 17:42:55 -0300

    fix problem when using normal if

--- script-generator/dangan-trial-sim/babel-plugin.js
+++ script-generator/dangan-trial-sim/babel-plugin.js
@@ -2,8 +2,13 @@ module.exports = ({ types: t }) => ({
 	name: "if-to-system-if",
 	visitor: {
 		IfStatement(path) {
-			const test = path.node.test;
+			// Verifica si tiene un comentario especial
+			const hasSystemComment = path.node.leadingComments?.some((c) =>
+				c.value.includes("@system"),
+			);
+			if (!hasSystemComment) return; // no hacer nada si no está el comentario
 
+			const test = path.node.test;
 			if (
 				t.isBinaryExpression(test) &&
 				test.operator === "===" &&
